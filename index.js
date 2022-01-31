19b0992cf5bfb4edc29d1198713e4b75e2310aa5
Phil Todisco <philtodisco@phils-mbp.lan>
2022-01-31 16:09:00 -0500

    Add GET request to StarCraft API

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
+// test command
 client.on('messageCreate', (message) => {
     let messageContent = message.content.toLowerCase()
     if (messageContent === 'test') {
@@ -23,6 +24,26 @@ client.on('messageCreate', (message) => {
     }
 });
 
+// GET data from StarCraft API
+client.on('messageCreate', (message) => {
+    let messageContent = message.content.toLowerCase()
+    if(messageContent === 'sc test') {
+        axios.get('https://starcraft-api.herokuapp.com/units')
+  .then(res => {
+    console.log('Status Code:', res.status);
+    console.log(res.data)
+
+    const units = res.data;
+
+    for(unit of units) {
+        message.channel.send(`Got user with id: ${unit._id}, name: ${unit.name}`);
+    } 
+  })
+  .catch(err => {
+    console.log('Error: ', err.message);
+  }); 
+    }
+});
 
 // Login to Discord with your client's token
 client.login(token);
