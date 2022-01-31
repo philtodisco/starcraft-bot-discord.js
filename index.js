ed2a1887acbc82378e4abf352d5cb7dd0f8498e8
Phil Todisco <philtodisco@phils-mbp.lan>
2022-01-31 14:52:15 -0500

    Add base code for bot

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+
+// Require the necessary discord.js classes
+const { Client, Intents } = require('discord.js');
+const token = process.env.TOKEN
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+// Login to Discord with your client's token
+client.login(token);
